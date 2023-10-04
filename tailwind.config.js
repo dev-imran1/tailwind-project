a12ceec16f0d5a981cb4b0915794f47f55338e65
Imran Hossain <imran.cit.bd@gmail.com>
2023-10-04 10:00:45 +0600

    navbar

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          'primary-color': '#FF7628',
+          'hover-color': '#D8EAFF',
+          'box-color': '#EBF7E9',
+          'text-color': '#202427',
+        },
+        fontFamily: {
+          'primary-font': "'Open Sans', sans-serif",
+          'secondary-font': "'Paprika', cursive",
+          'rare-font': "''Nunito', sans-serif",
+        },
+        fontSize: {
+          "18": '18px',
+          "20": '20px',
+          "24": '24px',
+          "30": '30px',
+          "48": '48px',
+          "72": '72px',
+        },
+        spacing: {
+          'container': '1170px',
+          'padding-btn': '24px 40px',
+        },
+      }
+    }
+  }
